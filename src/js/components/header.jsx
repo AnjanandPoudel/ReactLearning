7f7041d542604979aee90c3b90a8d4d5993513b9
Anjan <Cruserap@gmail.com>
2020-06-20 08:32:38 +0545

    uncontrolled forms

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
-import { Navbar ,NavbarBrand,Nav,NavbarToggler,Collapse,NavItem } from 'reactstrap';
-import {NavLink} from 'react-router-dom'
+import { Navbar ,NavbarBrand,Nav,NavbarToggler,Collapse,NavItem,Modal,ModalBody,ModalHeader, Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import {NavLink} from 'react-router-dom';
+
 
 
 
@@ -9,14 +10,28 @@ class Header extends Component{
         super()
         this.state={
             isNavOpen:false,
+            isModalOpen:false,
 
         }
-    }
+/*         this.handleSubmit=this.handleSubmit.bind(this)
+ */    }
 
     toggleNav=()=>{
         this.setState({
             isNavOpen:!this.state.isNavOpen
         })
+    }
+
+    toggleModal=()=>{
+        this.setState({
+            isModalOpen:!this.state.isModalOpen
+        })
+    }
+
+    handleLogin=(event)=>{
+        this.toggleModal()
+        alert("Username = "+this.username.value+" password = "+this.password.value +' Remember me = '+this.rememberme.checked);
+        event.preventDefault();
     }
 
     render(){
@@ -52,6 +67,13 @@ class Header extends Component{
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            <Nav className="ml-auto">
+                                <NavItem onClick={this.toggleModal}>
+                                    <Button type="submit" > Login |
+                                    <span className="fas fa-sign-in-alt"></span> </Button>
+                                    
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -61,12 +83,41 @@ class Header extends Component{
                         <div className="row row-header">
                             <div className="col-12 col-md-8">
                                 <h1>Anjan Poudel</h1>
-                                <p>This is  a paragraph. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, tenetur modi in consequuntur, doloremque sapiente illum quis maiores sequi aliquid neque odit aliquam veritatis. Quasi dolor sed ex dignissimos ipsa.</p>
+                                <p>This is  a paragraph. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, tent control the form that
+                                it renders:
+                                – Single source of truth
+                                – Tying the form state to component statetur modi in consequuntur, doloremque sapiente illum quis maiores sequi aliquid neque odit aliquam veritatis. Quasi dolor sed ex dignissimos ipsa.</p>
 
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} >
+                    <ModalHeader  toggle={this.toggleModal}>
+                        Login Form;
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" placeholder="username" id="username" name="username" innerRef={(input)=>this.username=input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" placeholder="password" id="password" name="password" innerRef={(input)=>this.password=input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" id="checkbox" name="rememberme" innerRef={(input)=>this.rememberme = input} /> 
+                                    Remember Me
+                                </Label>
+                                
+                            </FormGroup>
+                            <Button className="bg-success" type="submit" > Log in</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
 
             </div>
         </div>
